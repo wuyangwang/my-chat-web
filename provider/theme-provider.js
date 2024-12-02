eb6ce6435bc8d266d82e5246bbf74ee746689e3b
wuyangwang <wuyangtardis@gmail.com>
2024-12-02 10:49:58 +0800

    feat: update

---
+++ provider/theme-provider.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+export function ThemeProvider({ children, ...props }) {
+	return (
+		<NextThemesProvider
+			attribute='class'
+			defaultTheme='dark'
+			enableSystem
+			disableTransitionOnChange
+			{...props}
+		>
+			{children}
+		</NextThemesProvider>
+	)
+}
