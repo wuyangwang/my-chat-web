d3a2b1e8ac0668cb527fb5d64b6b2a7bfd5a3dd2
wuyangwang <wuyangtardis@gmail.com>
2024-12-03 17:22:16 +0800

    feat: update

--- actions/getModels.js
+++ actions/getModels.js
@@ -4,6 +4,11 @@ const host = process.env.NEXT_PUBLIC_HOST
 export async function getModels() {
 	const data = await fetch(`${host}/api/models`)
 	const res = await data.json()
+	// await sleep(5000)
 	if (res.code !== 200) return []
 	return res.data
+}
+
+async function sleep(ms = 1000) {
+	return new Promise((resolve) => setTimeout(resolve, ms))
 }
