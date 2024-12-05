8f97d3e64611135a3cb249da1d06de4661cd56f1
wuyangwang <wuyangtardis@gmail.com>
2024-12-05 09:02:15 +0800

    feat: update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import CanvasCursor from '@/components/anim/canvas-cursor'
 import TextWriter from '@/components/anim/text-writer'
-import { ModelToolList } from '@/components/home/ModelCard'
+import { ModelToolList } from '@/components/home/ModelToolList'
 
 export default function Home() {
 	return (
 		<div className='h-full justify-center'>
-			<div className='my-4 text-foreground text-4xl font-bold'>当前功能:</div>
+			<div className='my-4 text-center md:text-left text-foreground text-3xl font-bold'>
+				当前功能:
+			</div>
 			<ModelToolList />
 			<div className='text-center mt-10 text-2xl font-bold'>
 				<TextWriter text={'选择一个模型，开始加速吧！'} delay={0.2} loop />
