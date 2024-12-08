4f6cbe24b93eace625c22dfd89f1e2707150b0ae
wuyangwang <wuyangtardis@gmail.com>
2024-12-08 15:07:19 +0800

    feat: update

--- service/index.js
+++ service/index.js
@@ -4,13 +4,13 @@ export async function getModels(tag) {
 	return await apiGet('/api/models', { tag })
 }
 
-export async function getChat({ prompt, model }) {
+export async function getChat({ prompt, model = '' }) {
 	return await apiPost('/api/chat', { prompt, model }, { isStream: true })
 }
-export async function getTranslate({ text, source, target }) {
+export async function getTranslate({ text, source = '', target = '' }) {
 	return await apiGet('/api/trans', { text, source, target })
 }
-export async function getImage({ prompt, model }) {
+export async function getImage({ prompt, model = '' }) {
 	return await apiGet('/api/gen-image', { prompt, model }, { isImage: true })
 }
 export async function mock() {
