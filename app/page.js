1b372f2682a7d2208b3d45f9a697fb89c9af8366
wuyangwang <wuyangtardis@gmail.com>
2024-12-09 15:46:17 +0800

    feat: update

--- app/page.js
+++ app/page.js
@@ -5,12 +5,9 @@ export default function Home() {
 	return (
 		<div className='p-4 h-full justify-center'>
 			<div className='my-4 text-center md:text-left text-foreground text-3xl font-bold'>
-				当前功能:
+				<TextWriter text={'选择一个模型，开始聊天！'} delay={0.2} loop />
 			</div>
 			<ModelToolList />
-			<div className='text-center mt-10 text-2xl font-bold'>
-				<TextWriter text={'选择一个模型，开始加速吧！'} delay={0.2} loop />
-			</div>
 		</div>
 	)
 }
