c61f70cbfd6a1243ef7416b038c4d03e68c5715d
wuyangwang <wuyangtardis@gmail.com>
2024-12-10 09:35:29 +0800

    feat: update

--- components/analysis/Google.jsx
+++ components/analysis/Google.jsx
@@ -11,15 +11,19 @@ export function GoogleAnalysis() {
 				strategy='lazyOnload'
 				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
 			/>
-			<Script id='google-analytics' strategy='afterInteractive'>
-				{`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+			<Script
+				id='google-analytics'
+				strategy='afterInteractive'
+				dangerouslySetInnerHTML={{
+					__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
 
-        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
-        `}
-			</Script>
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+          `
+				}}
+			/>
 		</>
 	)
 }
