ddb99971b9c687af2200b31e87ed8d924a5fb56b
wjt <wuyangtardis@gmail.com>
2024-12-16 11:06:01 +0800

    feat: update

--- service/index.js
+++ service/index.js
@@ -1,3 +1,6 @@
+import { ModelTypeEnum } from '@/utils'
+import { chatWithApi } from './api'
+
 export * from './gemini'
 export * from './grok'
 export * from './ollama'
@@ -8,4 +11,12 @@ export async function mock() {
 	return new Promise((resolve) => {
 		setTimeout(() => resolve({ text: '注意：开发环境使用mock接口数据', url: 'xxx' }), 2000)
 	})
+}
+
+export const getChatApi = (type) => {
+	if (type === ModelTypeEnum.gemini) return chatWithGemini
+	if (type === ModelTypeEnum.grok) return chatWithGrok
+	if (type === ModelTypeEnum.ollama) return chatWithOllama
+	if (type === ModelTypeEnum.openai) return chatWithOpenAI
+	return chatWithApi
 }
